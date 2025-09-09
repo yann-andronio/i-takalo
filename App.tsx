efd3daaff4d433085280ea0f82a6024e95328a14
Julio23F <juliofaralahy23@gmail.com>
2025-09-09 15:53:51 +0300

    Debug

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 import './global.css'; // NativeWind
 import { LogBox } from 'react-native';
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
 import { ProductProvider } from './src/context/ProductContext';
 import { UserProvider } from './src/context/UserContext';
 // Masque le warning de SafeAreaView
--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import EncryptedStorage from "react-native-encrypted-storage";
 
 const API = axios.create({
-  baseURL: "http://192.168.88.22:8000/", 
+  baseURL: "https://ant-stewart-want-yang.trycloudflare.com/", 
   timeout: 10000,
 });
 

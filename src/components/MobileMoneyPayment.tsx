34713ea1ba4545062da3fb55bc2f4d30eef3e965
Yann Andronio <todiveloyannandronio@gmail.com>
2025-09-29 22:08:07 +0300

    nametaka sary mode de paiement

--- src/components/MobileMoneyPayment.tsx
+++ src/components/MobileMoneyPayment.tsx
@@ -1,6 +1,18 @@
-import { View, Text, TouchableOpacity, TextInput, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  ImageBackground,
+} from 'react-native';
 import React, { useState } from 'react';
-import { PhoneIcon, UserIcon, HashIcon, CheckCircleIcon } from 'phosphor-react-native';
+import {
+  PhoneIcon,
+  UserIcon,
+  HashIcon,
+  CheckCircleIcon,
+} from 'phosphor-react-native';
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -15,13 +27,22 @@ type FormData = {
 };
 
 const validationSchema = yup.object({
-  phoneNumber: yup.string().required('Le numéro est obligatoire').matches(/^(032|033|034|038)\d{7}$/, 'Numéro invalide (ex: 0341234567)'),
+  phoneNumber: yup
+    .string()
+    .required('Le numéro est obligatoire')
+    .matches(/^(032|033|034|038)\d{7}$/, 'Numéro invalide (ex: 0341234567)'),
   nomcompte: yup.string().required('Le nom du compte est obligatoire'),
   reference: yup.string().required('La référence est obligatoire'),
 });
 
-
-const InputField = ({ label, icon: Icon, control, name, placeholder, keyboardType }: any) => (
+const InputField = ({
+  label,
+  icon: Icon,
+  control,
+  name,
+  placeholder,
+  keyboardType,
+}: any) => (
   <View className="mb-4">
     <Text className="mb-1 text-sm font-semibold text-gray-700">{label}</Text>
     <View className="flex-row items-center px-3 py-[0.5%] border border-gray-300 rounded-lg bg-gray-50">
@@ -45,22 +66,40 @@ const InputField = ({ label, icon: Icon, control, name, placeholder, keyboardTyp
 );
 
 export default function MobileMoneyPayment() {
-  const [selectedOperator, setSelectedOperator] = useState<Operator | null>(null);
+  const [selectedOperator, setSelectedOperator] = useState<Operator | null>(
+    null,
+  );
   const [errorOperator, setErrorOperator] = useState(false);
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
-  const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
     resolver: yupResolver(validationSchema),
     // 'onChange' met à jour 'isValid' à chaque changement de champ
-    mode: 'onChange', 
+    mode: 'onChange',
   });
 
-   const isButtonDisabled = !isValid || !selectedOperator;
+  const isButtonDisabled = !isValid || !selectedOperator;
 
   const operators = [
-    { id: 'telma', label: 'MVola', logo: 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Telma_logo.png' },
-    { id: 'airtel', label: 'Airtel Money', logo: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Airtel_logo_2010.png' },
-    { id: 'orange', label: 'Orange Money', logo: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Orange_logo.svg' },
+    {
+      id: 'telma',
+      label: 'MVola',
+      logo: require('../assets/images/telma.jpeg'),
+    },
+    {
+      id: 'airtel',
+      label: 'Airtel Money',
+      logo: require('../assets/images/airtel.jpeg'),
+    },
+    {
+      id: 'orange',
+      label: 'Orange Money',
+      logo: require('../assets/images/orange.jpeg'),
+    },
   ];
 
   const onSubmit = (data: FormData) => {
@@ -68,22 +107,26 @@ export default function MobileMoneyPayment() {
       setErrorOperator(true);
       return;
     }
-    console.log('Formulaire valide  sady vita achat', { ...data, selectedOperator });
+    console.log('Formulaire valide  sady vita achat', {
+      ...data,
+      selectedOperator,
+    });
     Toast.show({
       type: 'success',
       text1: 'Paiement en cours',
       text2: `Veuillez confirmer la transaction sur votre téléphone (${selectedOperator.toUpperCase()}).`,
-      visibilityTime: 4000, 
+      visibilityTime: 4000,
     });
-  setTimeout(() => {
-  navigation.goBack(); 
-}, 3000); 
-  
+    setTimeout(() => {
+      navigation.goBack();
+    }, 3000);
   };
 
   return (
     <View className="flex-1 px-6 py-5 bg-white">
-      <Text className="mb-4 text-lg font-bold text-gray-800">Choisir un opérateur Mobile Money</Text>
+      <Text className="mb-4 text-lg font-bold text-gray-800">
+        Choisir un opérateur Mobile Money
+      </Text>
 
       <View className="flex-row justify-between mb-4">
         {operators.map(op => (
@@ -93,41 +136,103 @@ export default function MobileMoneyPayment() {
               setSelectedOperator(op.id as Operator);
               setErrorOperator(false);
             }}
-            className={`flex-1 mx-1 items-center p-3 rounded-xl border ${selectedOperator === op.id ? 'border-[#03233A] bg-[#03233A]/5' : 'border-gray-200 bg-gray-50'}`}
+            className={`flex-1 mx-1 items-center rounded-xl overflow-hidden border ${
+              selectedOperator === op.id
+                ? 'border-[#03233A]'
+                : 'border-gray-200'
+            }`}
           >
-            <Image source={{ uri: op.logo }} className="w-10 h-10 mb-1" resizeMode="contain" />
-            <Text className={`text-[11px] font-semibold ${selectedOperator === op.id ? 'text-[#03233A]' : 'text-gray-600'}`}>{op.label}</Text>
+            <ImageBackground
+              source={op.logo}
+              resizeMode="cover"
+              className="w-full relative h-24 justify-end items-center"
+              imageStyle={{ borderRadius: 12, opacity: 1 }}
+            >
+              {selectedOperator === op.id ? (
+                <View className="absolute top-2 right-2">
+                  <CheckCircleIcon size={20} weight="fill" color="white" />
+                </View>
+              ) : null}
+
+              {/* <View
+            
+                className="p-2 w-full items-center"
+                style={{
+                  backgroundColor:
+                    selectedOperator === op.id
+                      ? 'rgba(3, 35, 58, 0.7)'
+                      : 'rgba(0, 0, 0, 0.5)',
+                }}
+              >
+                <Text className={`text-xs font-semibold text-white`}>
+                  {op.label}
+                </Text>
+              </View> */}
+            </ImageBackground>
           </TouchableOpacity>
         ))}
       </View>
 
-     
       {errorOperator && (
         <View className="mb-4 flex-row items-center bg-red-100 border border-red-400 px-3 py-2 rounded-lg">
-          <Text className="text-red-700 font-medium text-sm">⚠️ Veuillez choisir un opérateur</Text>
+          <Text className="text-red-700 font-medium text-sm">
+            ⚠️ Veuillez choisir un opérateur
+          </Text>
         </View>
       )}
 
-    
-      <InputField label="Numéro de téléphone" icon={PhoneIcon} control={control} name="phoneNumber" placeholder="Ex: 0341234567" keyboardType="phone-pad" />
-      {errors.phoneNumber && <Text className="mb-2 text-xs text-red-500">{errors.phoneNumber.message}</Text>}
+      <InputField
+        label="Numéro de téléphone"
+        icon={PhoneIcon}
+        control={control}
+        name="phoneNumber"
+        placeholder="Ex: 0341234567"
+        keyboardType="phone-pad"
+      />
+      {errors.phoneNumber && (
+        <Text className="mb-2 text-xs text-red-500">
+          {errors.phoneNumber.message}
+        </Text>
+      )}
 
-      <InputField label="Nom du compte" icon={UserIcon} control={control} name="nomcompte" placeholder="Nom complet du titulaire" />
-      {errors.nomcompte && <Text className="mb-2 text-xs text-red-500">{errors.nomcompte.message}</Text>}
+      <InputField
+        label="Nom du compte"
+        icon={UserIcon}
+        control={control}
+        name="nomcompte"
+        placeholder="Nom complet du titulaire"
+      />
+      {errors.nomcompte && (
+        <Text className="mb-2 text-xs text-red-500">
+          {errors.nomcompte.message}
+        </Text>
+      )}
 
-      <InputField label="Référence" icon={HashIcon} control={control} name="reference" placeholder="Ex: 05191698" />
-      {errors.reference && <Text className="mb-2 text-xs text-red-500">{errors.reference.message}</Text>}
+      <InputField
+        label="Référence"
+        icon={HashIcon}
+        control={control}
+        name="reference"
+        placeholder="Ex: 05191698"
+      />
+      {errors.reference && (
+        <Text className="mb-2 text-xs text-red-500">
+          {errors.reference.message}
+        </Text>
+      )}
 
-     <TouchableOpacity
-    onPress={handleSubmit(onSubmit)}
-    disabled={isButtonDisabled} 
-    className={`mt-6 flex-row items-center justify-center py-3 rounded-xl shadow-md ${isButtonDisabled ? 'bg-gray-400' : 'bg-[#03233A] active:opacity-90'}`}
->
-    <CheckCircleIcon size={22} color="white" weight="bold" />
-    <Text className="ml-2 text-white font-semibold text-base">Confirmer le paiement</Text>
-</TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleSubmit(onSubmit)}
+        disabled={isButtonDisabled}
+        className={`mt-6 flex-row items-center justify-center py-3 rounded-xl shadow-md ${
+          isButtonDisabled ? 'bg-gray-400' : 'bg-[#03233A] active:opacity-90'
+        }`}
+      >
+        <CheckCircleIcon size={22} color="white" weight="bold" />
+        <Text className="ml-2 text-white font-semibold text-base">
+          Confirmer le paiement
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-

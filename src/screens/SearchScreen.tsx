53d0a8004f54e1614d7171f35d834c84568777ff
Julio23F <juliofaralahy23@gmail.com>
2025-10-17 00:18:26 +0300

    Update: change API_BASE_URL

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -10,8 +10,9 @@ import SearchBar from '../components/SearchBar';
 import { ArrowLeftIcon } from 'phosphor-react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useDebounce } from '../hooks/useDebounce';
+import { API_BASE_URL } from '@env';
 
-const API_BASE_URL = 'https://distances-eau-attempt-impose.trycloudflare.com'; 
+// const API_BASE_URL = 'https://distances-eau-attempt-impose.trycloudflare.com'; 
 
 export default function SearchScreen() {
   const [search, setSearch] = useState('');
